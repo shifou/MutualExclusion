80ea25f368f2111862b39d909aeab1112d591b78
chanjuaz <tshingcj@gmail.com>
2015-02-22 16:02:12 -0500

    sdfsdf

--- src/Mutex.java
+++ src/Mutex.java
@@ -189,10 +189,6 @@ public class Mutex {
 		if(!votes.contains(mes.src)&&voteMem.contains(mes.src))
 			votes.add(mes.src);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1819f1bc7da9083bfb61284856bdfa728ab24bf4
 		System.out.println("have votes: "+ this.vote+" total need: "+this.groupSize);
 		
 		if(vote==groupSize)
